55718a4f91670d07b3ed1a93ebb8018eb064597d
wangyinhe <wyinhe@sina.cn>
2026-05-20 15:18:39 +0800

    hash: 缺失的第一个正数

---
+++ src/hash/first_missing_positive.rs
@@ -0,0 +1,76 @@
+/// [41. 缺失的第一个正数](https://leetcode.cn/problems/first-missing-positive/)
+struct Solution;
+
+use std::collections::HashSet;
+impl Solution {
+    pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
+        let set: HashSet<i32> = HashSet::from_iter(nums.into_iter());
+        let mut ans = 1;
+        while set.contains(&ans) {
+            ans += 1;
+        }
+        ans
+    }
+
+    pub fn first_missing_positive2(mut nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        for i in 0..n {
+            while nums[i] > 0 && nums[i] as usize <= n {
+                let cur = nums[i] as usize;
+                if cur == i + 1 || cur as i32 == nums[cur - 1] {
+                    break;
+                }
+                nums.swap(i, cur - 1);
+            }
+        }
+        for (i, &num) in nums.iter().enumerate() {
+            if num as usize != i + 1 {
+                return i as i32 + 1;
+            }
+        }
+        n as i32 + 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let nums = vec![1, 2, 0];
+        let ans = 3;
+        assert_eq!(Solution::first_missing_positive(nums), ans);
+
+        let nums = vec![3, 4, -1, 1];
+        let ans = 2;
+        assert_eq!(Solution::first_missing_positive(nums), ans);
+
+        let nums = vec![7, 8, 9, 11, 12];
+        let ans = 1;
+        assert_eq!(Solution::first_missing_positive(nums), ans);
+
+        let nums = vec![1, 1];
+        let ans = 2;
+        assert_eq!(Solution::first_missing_positive(nums), ans);
+    }
+
+    #[test]
+    fn test2() {
+        let nums = vec![1, 2, 0];
+        let ans = 3;
+        assert_eq!(Solution::first_missing_positive2(nums), ans);
+
+        let nums = vec![3, 4, -1, 1];
+        let ans = 2;
+        assert_eq!(Solution::first_missing_positive2(nums), ans);
+
+        let nums = vec![7, 8, 9, 11, 12];
+        let ans = 1;
+        assert_eq!(Solution::first_missing_positive2(nums), ans);
+
+        let nums = vec![1, 1];
+        let ans = 2;
+        assert_eq!(Solution::first_missing_positive2(nums), ans);
+    }
+}
--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -1,2 +1,4 @@
+pub mod first_missing_positive;
+pub mod is_anagram;
 pub mod randomized_set;
 pub mod ransom_note;
