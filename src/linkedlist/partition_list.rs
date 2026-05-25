a166b37fbb1855ac4d86b378b666f3e37eae9e73
wangyinhe <wyinhe@sina.cn>
2026-05-25 17:37:58 +0800

    链表：分隔链表、重排链表

---
+++ src/linkedlist/partition_list.rs
@@ -0,0 +1,49 @@
+use crate::linkedlist::ListNode;
+/// [86. 分隔链表](https://leetcode.cn/problems/partition-list/)
+struct Solution;
+
+impl Solution {
+    pub fn partition(head: Option<Box<ListNode>>, x: i32) -> Option<Box<ListNode>> {
+        let mut dummy1 = ListNode::new(0);
+        let mut dummy2 = ListNode::new(0);
+        let mut p1 = &mut dummy1;
+        let mut p2 = &mut dummy2;
+        let mut cur = head;
+        while let Some(mut node) = cur {
+            let next = node.next.take(); // 先把下一个节点取走暂存起来
+            if node.val < x {
+                p1.next = Some(node);
+                p1 = p1.next.as_mut().unwrap();
+            } else {
+                p2.next = Some(node);
+                p2 = p2.next.as_mut().unwrap();
+            }
+            cur = next;
+        }
+        p1.next = dummy2.next;
+        dummy1.next
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let l = ListNode::from_vec(vec![1, 4, 3, 2, 5, 2]);
+        let x = 3;
+        let ans = ListNode::from_vec(vec![1, 2, 2, 4, 3, 5]);
+        assert_eq!(Solution::partition(l, x), ans);
+
+        let l = ListNode::from_vec(vec![2, 1]);
+        let x = 2;
+        let ans = ListNode::from_vec(vec![1, 2]);
+        assert_eq!(Solution::partition(l, x), ans);
+
+        let l = ListNode::from_vec(vec![]);
+        let x = 2;
+        let ans = ListNode::from_vec(vec![]);
+        assert_eq!(Solution::partition(l, x), ans);
+    }
+}
---
+++ src/linkedlist/reorder_list.rs
@@ -0,0 +1,73 @@
+use crate::linkedlist::ListNode;
+/// [143. 重排链表](https://leetcode.cn/problems/reorder-list/description/)
+struct Solution;
+
+impl Solution {
+    pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
+        if head.is_none() {
+            return;
+        }
+        // 分离出后半段
+        let mut slow_cnt = 0;
+        let mut fast = head.as_deref().unwrap().next.as_deref();
+        while let Some(node) = fast {
+            if let Some(f_next) = node.next.as_deref() {
+                fast = f_next.next.as_deref();
+                slow_cnt += 1;
+            } else {
+                break;
+            }
+        }
+        let mut p = head.as_deref_mut().unwrap();
+        for _ in 0..slow_cnt {
+            p = p.next.as_deref_mut().unwrap();
+        }
+        let mut l2 = p.next.take();
+
+        // 反转l2
+        let mut prev = None;
+        while let Some(mut node) = l2 {
+            let next = node.next.take();
+            node.next = prev;
+            prev = Some(node);
+            l2 = next;
+        }
+
+        // 合并
+        // head始终不被改变，避免“所有权真空期”
+        let mut l1 = head.as_mut().unwrap().next.take();
+        let mut l2 = prev;
+        let mut l = head.as_deref_mut().unwrap();
+        while l1.is_some() && l2.is_some() {
+            let mut node1 = l1.take().unwrap();
+            let mut node2 = l2.take().unwrap();
+
+            let l1_next = node1.next.take();
+            let l2_next = node2.next.take();
+            node2.next = Some(node1);
+            l.next = Some(node2);
+            l = l.next.as_deref_mut().unwrap().next.as_deref_mut().unwrap();
+            l1 = l1_next;
+            l2 = l2_next;
+        }
+        l.next = if l1.is_some() { l1 } else { l2 };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut l = ListNode::from_vec(vec![1, 2, 3, 4]);
+        let ans = ListNode::from_vec(vec![1, 4, 2, 3]);
+        Solution::reorder_list(&mut l);
+        assert_eq!(l, ans);
+
+        let mut l = ListNode::from_vec(vec![1, 2, 3, 4, 5]);
+        let ans = ListNode::from_vec(vec![1, 5, 2, 4, 3]);
+        Solution::reorder_list(&mut l);
+        assert_eq!(l, ans);
+    }
+}
