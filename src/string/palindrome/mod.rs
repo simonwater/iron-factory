6fcadab0014cdc76b82c6280afa9b2ae3fa5b43d
wangyinhe <wyinhe@sina.cn>
2026-05-27 17:55:38 +0800

    字符串-回文：统计回文子串数量。中心扩散+动态规划

--- src/string/palindrome/mod.rs
+++ src/string/palindrome/mod.rs
@@ -1,3 +1,4 @@
 pub mod is_palindrome;
 pub mod longest_palindrome;
+pub mod palindromic_substrings;
 pub mod valid_palindrome;
---
+++ src/string/palindrome/palindromic_substrings.rs
@@ -0,0 +1,73 @@
+/// [647. 回文子串](https://leetcode.cn/problems/palindromic-substrings/)
+struct Solution;
+
+impl Solution {
+    /// 中心扩散法
+    pub fn count_substrings(s: String) -> i32 {
+        let mut ans = 0;
+        let s_bytes = s.as_bytes();
+        for i in 0..s_bytes.len() {
+            ans += Self::calc(s_bytes, i, i);
+            ans += Self::calc(s_bytes, i, i + 1);
+        }
+        ans
+    }
+
+    pub fn count_substrings2(s: String) -> i32 {
+        let s_bytes = s.as_bytes();
+        (0..s_bytes.len())
+            .map(|i| Self::calc(s_bytes, i, i) + Self::calc(s_bytes, i, i + 1))
+            .sum()
+    }
+
+    fn calc(s_bytes: &[u8], mut i: usize, mut j: usize) -> i32 {
+        let mut cnt = 0;
+        while i >= 0 && j < s_bytes.len() {
+            if s_bytes[i] == s_bytes[j] {
+                cnt += 1;
+                if i == 0 {
+                    break;
+                }
+                i -= 1;
+                j += 1;
+            } else {
+                break;
+            }
+        }
+        cnt
+    }
+}
+
+struct Solution2;
+/// 动态规划
+impl Solution2 {
+    pub fn count_substrings(s: String) -> i32 {
+        let mut ans = 0;
+        let s_bytes = s.as_bytes();
+        let n = s_bytes.len();
+        // dp[i][j] 表示字符串s[i..=j]是否是回文
+        let mut dp = vec![vec![false; n]; n];
+        for j in 0..n {
+            // 先循环j，也就是尾部
+            dp[j][j] = true;
+            ans += 1;
+            for i in 0..j {
+                if s_bytes[i] == s_bytes[j] {
+                    dp[i][j] = if j <= i + 1 { true } else { dp[i + 1][j - 1] };
+                    if dp[i][j] {
+                        ans += 1;
+                    }
+                }
+            }
+        }
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
