0b7f629cb7c66698d726dd29f99f54770420a40a
wangyinhe <wyinhe@sina.cn>
2026-05-27 18:23:00 +0800

    哈希：同构字符串

---
+++ src/hash/isomorphic_strings.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+
+/// [205. 同构字符串](https://leetcode.cn/problems/isomorphic-strings/)
+struct Solution;
+
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        if s.len() != t.len() || s.is_empty() || t.is_empty() {
+            return false;
+        }
+        let s_bytes = s.as_bytes();
+        let t_bytes = t.as_bytes();
+        let mut s2t_map = HashMap::with_capacity(128);
+        let mut t2s_map = HashMap::with_capacity(128);
+        for (&s, &t) in s_bytes.iter().zip(t_bytes.iter()) {
+            if *s2t_map.entry(s).or_insert(t) != t {
+                return false;
+            }
+            if *t2s_map.entry(t).or_insert(s) != s {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -1,6 +1,7 @@
 pub mod first_missing_positive;
 pub mod happy_number;
 pub mod is_anagram;
+pub mod isomorphic_strings;
 pub mod randomized_set;
 pub mod ransom_note;
 pub mod word_pattern;
