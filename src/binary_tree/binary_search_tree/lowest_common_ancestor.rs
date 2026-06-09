56ca263acf1aaf94e75593961478c8ffb05ba6f4
wangyinhe <wyinhe@sina.cn>
2026-06-09 22:33:40 +0800

    二叉搜索树的最近公共祖先

---
+++ src/binary_tree/binary_search_tree/lowest_common_ancestor.rs
@@ -0,0 +1,50 @@
+use crate::binary_tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+/// [235. 二叉搜索树的最近公共祖先](https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/)
+struct Solution;
+
+impl Solution {
+    pub fn lowest_common_ancestor(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut p_val = p.as_ref().unwrap().borrow().val;
+        let mut q_val = q.as_ref().unwrap().borrow().val;
+        if p_val > q_val {
+            std::mem::swap(&mut p_val, &mut q_val);
+        }
+
+        Self::dfs(&root, p_val, q_val)
+    }
+
+    fn dfs(
+        root: &Option<Rc<RefCell<TreeNode>>>,
+        p_val: i32,
+        q_val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if let Some(root_rc) = root {
+            let node = root_rc.borrow();
+            if node.val < p_val {
+                return Self::dfs(&node.right, p_val, q_val);
+            } else if node.val > q_val {
+                return Self::dfs(&node.left, p_val, q_val);
+            } else {
+                return Some(Rc::clone(root_rc));
+            }
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tree;
+
+    #[test]
+    fn test1() {}
+}
--- src/binary_tree/binary_search_tree/mod.rs
+++ src/binary_tree/binary_search_tree/mod.rs
@@ -1,3 +1,4 @@
+pub mod lowest_common_ancestor;
 pub mod minimum_difference;
 pub mod my_calendar_i;
 pub mod search_in_bst;
