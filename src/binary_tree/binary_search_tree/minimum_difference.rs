1bbc4101ecf4d0277a029155e33a83b96be4e38d
wangyinhe <wyinhe@sina.cn>
2026-06-09 18:11:29 +0800

    二叉搜索树的最小绝对差

---
+++ src/binary_tree/binary_search_tree/minimum_difference.rs
@@ -0,0 +1,34 @@
+use crate::binary_tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+
+/// [530. 二叉搜索树的最小绝对差](https://leetcode.cn/problems/minimum-absolute-difference-in-bst/description/)
+struct Solution;
+
+impl Solution {
+    pub fn get_minimum_difference(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut prev = i32::MIN / 2;
+        let mut ans = i32::MAX;
+        Self::dfs(&root, &mut prev, &mut ans);
+        ans
+    }
+
+    fn dfs(root: &Option<Rc<RefCell<TreeNode>>>, prev: &mut i32, ans: &mut i32) {
+        if let Some(root_rc) = root {
+            let node = root_rc.borrow();
+            Self::dfs(&node.left, prev, ans);
+            *ans = (*ans).min(node.val - (*prev));
+            *prev = node.val;
+            Self::dfs(&node.right, prev, ans);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tree;
+
+    #[test]
+    fn test1() {}
+}
--- src/binary_tree/binary_search_tree/mod.rs
+++ src/binary_tree/binary_search_tree/mod.rs
@@ -1,2 +1,3 @@
+pub mod minimum_difference;
 pub mod my_calendar_i;
 pub mod search_in_bst;
