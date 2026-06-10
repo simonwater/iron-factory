3890ed212d6cdb72a934b2c709ff52dfe3ac5357
wangyinhe <wyinhe@sina.cn>
2026-06-10 12:12:57 +0800

    实现前缀树

--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,5 @@ pub mod math;
 pub mod rust;
 pub mod stack;
 pub mod string;
+pub mod trie;
 pub mod uncategory;
---
+++ src/trie/trie.rs
@@ -0,0 +1,52 @@
+/// [208. 实现 Trie (前缀树)](https://leetcode.cn/problems/implement-trie-prefix-tree/)
+const INIT: Option<Box<Trie>> = None;
+
+struct Trie {
+    pub is_end: bool,
+    pub datas: [Option<Box<Trie>>; 26],
+}
+
+impl Trie {
+    fn new() -> Self {
+        Self {
+            is_end: false,
+            datas: [INIT; 26],
+        }
+    }
+
+    fn insert(&mut self, word: String) {
+        let bytes = word.as_bytes();
+        let mut t = self;
+        for &c in bytes {
+            let idx = (c - b'a') as usize;
+            if t.datas[idx].is_none() {
+                t.datas[idx] = Some(Box::new(Trie::new()));
+            }
+            t = t.datas[idx].as_deref_mut().unwrap();
+        }
+        t.is_end = true;
+    }
+
+    fn search(&self, word: String) -> bool {
+        self.find(&word).is_some_and(|t| t.is_end)
+    }
+
+    fn starts_with(&self, prefix: String) -> bool {
+        self.find(&prefix).is_some()
+    }
+
+    fn find(&self, s: &str) -> Option<&Trie> {
+        let bytes = s.as_bytes();
+        let mut t = self;
+        for &c in bytes {
+            let idx = (c - b'a') as usize;
+            if t.datas[idx].is_none() {
+                return None;
+            } else {
+                t = t.datas[idx].as_deref().unwrap();
+            }
+        }
+
+        Some(t)
+    }
+}
