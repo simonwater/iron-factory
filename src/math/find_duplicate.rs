4e12b8466cb1e182789f0e5760aba03f5cd6cc2c
wangyinhe <wyinhe@sina.cn>
2026-06-13 15:02:01 +0800

    寻找重复数

---
+++ src/math/find_duplicate.rs
@@ -0,0 +1,33 @@
+/// # [287. 寻找重复数](https://leetcode.cn/problems/find-the-duplicate-number/)
+///
+
+struct Solution;
+
+impl Solution {
+    // 环状链表寻找环入口
+    pub fn find_duplicate(nums: Vec<i32>) -> i32 {
+        let mut slow = 0;
+        let mut fast = 0;
+        loop {
+            slow = nums[slow] as usize;
+            fast = nums[nums[fast] as usize] as usize;
+            if slow == fast {
+                break;
+            }
+        }
+        let mut p = 0;
+        while p != slow {
+            p = nums[p] as usize;
+            slow = nums[slow] as usize;
+        }
+        p as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
