c8752a45e6933b62793cdb49cb2b828b0390c055
wangyinhe <wyinhe@sina.cn>
2026-06-13 23:02:21 +0800

    动态规划：分割等和子集 ，状态空间压缩

--- src/dp/bag_combine/can_partition.rs
+++ src/dp/bag_combine/can_partition.rs
@@ -1,10 +1,10 @@
 /// # [416. 分割等和子集](https://leetcode.cn/problems/partition-equal-subset-sum/)
 ///
-/// 状态定义：`dp[amt][i]` 表示nums中前i个是否能构成amt
+/// 状态定义：`dp[i][amt]` 表示nums中前i个是否能构成amt
 struct Solution;
 
 impl Solution {
-    // 
+    //
     pub fn can_partition(nums: Vec<i32>) -> bool {
         let mut target = nums.iter().sum::<i32>() as usize;
         if target % 2 == 1 {
@@ -12,21 +12,49 @@ impl Solution {
         }
         target = target / 2;
         let n = nums.len();
-        let mut dp = vec![vec![false; n + 1]; target + 1];
-        for val in dp[0].iter_mut() {
-            *val = true;
-        }
-        for amt in 1..=target {
-            for i in 1..=n {
-                let num = nums[i - 1] as usize;
+        let mut dp = vec![vec![false; target + 1]; n + 1];
+        dp[0][0] = true;
+        for i in 1..=n {
+            dp[i][0] = true;
+            let num = nums[i - 1] as usize;
+            for amt in 1..=target {
                 if num > amt {
-                    dp[amt][i] = dp[amt][i - 1];
+                    dp[i][amt] = dp[i - 1][amt];
                 } else {
-                    dp[amt][i] = dp[amt][i - 1] || dp[amt - num][i - 1];
+                    dp[i][amt] = dp[i - 1][amt] || dp[i - 1][amt - num];
                 }
             }
         }
-        dp[target][n]
+        dp[n][target]
+    }
+}
+
+/// # 状态压缩
+/// 可以发现在计算 dp 的过程中，每一行的 dp 值都只与上一行的 dp 值有关，因此只需要一个一维数组即可将空间复杂度降到 O(target)。
+/// 此时的转移方程为:<br>
+/// `dp[j]=dp[j] ∣ dp[j−nums[i]]` <br>
+/// 且需要注意的是第二层的循环我们需要从大到小计算，因为如果我们从小到大更新 dp 值，
+/// 那么在计算 dp[j] 值的时候，dp[j−nums[i]] 已经是被更新过的状态，不再是上一行的 dp 值。
+struct Solution2;
+
+impl Solution2 {
+    //
+    pub fn can_partition(nums: Vec<i32>) -> bool {
+        let mut target = nums.iter().sum::<i32>() as usize;
+        if target % 2 == 1 {
+            return false;
+        }
+        target = target / 2;
+        let n = nums.len();
+        let mut dp = vec![false; target + 1];
+        dp[0] = true;
+        for i in 0..n {
+            let num = nums[i] as usize;
+            for amt in (num..=target).rev() {
+                dp[amt] = dp[amt] || dp[amt - num];
+            }
+        }
+        dp[target]
     }
 }
 
