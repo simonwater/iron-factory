f91cc4b687500591833b03ed25f77260a45f02a7
wangyinhe <wyinhe@sina.cn>
2026-06-15 18:34:10 +0800

    以bus为节点建图

--- src/graph/bus_routes.rs
+++ src/graph/bus_routes.rs
@@ -52,6 +52,57 @@ impl Solution {
     }
 }
 
+/// 以bus为图节点，相邻节点为：当前bus经过的所有车站上经过的bus
+struct Solution2;
+
+impl Solution2 {
+    //
+    pub fn num_buses_to_destination(routes: Vec<Vec<i32>>, source: i32, target: i32) -> i32 {
+        if source == target {
+            return 0;
+        }
+        let mut stop2bus: HashMap<i32, Vec<usize>> = HashMap::with_capacity(32);
+        for (bus, stops) in routes.iter().enumerate() {
+            for &stop in stops {
+                stop2bus
+                    .entry(stop)
+                    .or_insert_with(|| Vec::with_capacity(32))
+                    .push(bus);
+            }
+        }
+        if !stop2bus.contains_key(&source) || !stop2bus.contains_key(&target) {
+            return -1;
+        }
+        let n = routes.len();
+        let mut bus_visited = vec![false; n];
+        let mut q = VecDeque::with_capacity(n);
+        if let Some(start_buses) = stop2bus.get(&source) {
+            for &bus in start_buses {
+                bus_visited[bus] = true;
+                q.push_back((bus, 1));
+            }
+        }
+
+        while let Some((cur_bus, bus_cnt)) = q.pop_front() {
+            for &stop in &routes[cur_bus] {
+                if stop == target {
+                    return bus_cnt;
+                }
+                if let Some(next_buses) = stop2bus.get(&stop) {
+                    for &next_bus in next_buses {
+                        if !bus_visited[next_bus] {
+                            bus_visited[next_bus] = true;
+                            q.push_back((next_bus, bus_cnt + 1));
+                        }
+                    }
+                }
+            }
+        }
+
+        -1
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
