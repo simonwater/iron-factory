76947989e6d8d553569d53ac47c0f701bd6b1d64
wangyinhe <wyinhe@sina.cn>
2026-06-17 14:29:14 +0800

    1. 乘积最大子数组。动态规划 2.最长递增子序列。动态规划+二分查找

---
+++ src/dp/length_of_lis.rs
@@ -0,0 +1,75 @@
+/// # [300. 最长递增子序列](https://leetcode.cn/problems/longest-increasing-subsequence/)
+///
+
+/// 动态规划：dp[i]表示以nums[i]结尾的最长递增子序列长度，中间记录全局最大长度值
+struct Solution;
+
+impl Solution {
+    //
+    pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut dp = vec![1; n];
+        let mut ans = 1;
+        for i in 1..n {
+            let cur = nums[i];
+            for j in 0..i {
+                if nums[j] < cur {
+                    dp[i] = dp[i].max(dp[j] + 1);
+                }
+            }
+            ans = ans.max(dp[i]);
+        }
+        ans
+    }
+}
+
+struct Solution2;
+
+impl Solution2 {
+    //
+    pub fn length_of_lis(nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        let mut tails: Vec<i32> = Vec::with_capacity(n);
+        for num in nums {
+            let i = Self::search_lower_bound(&tails, num);
+            if i == tails.len() {
+                tails.push(num);
+            } else {
+                tails[i] = num;
+            }
+        }
+
+        tails.len() as i32
+    }
+
+    // 查找大于等于x的第一个位置
+    fn search_lower_bound(nums: &[i32], x: i32) -> usize {
+        if nums.is_empty() {
+            return 0;
+        }
+        let mut lo = 0;
+        let mut hi = nums.len() - 1;
+        let mut ans = nums.len();
+        while lo <= hi {
+            let mid = lo + ((hi - lo) >> 1);
+            if nums[mid] >= x {
+                ans = mid;
+                if hi == 0 {
+                    break;
+                }
+                hi -= 1;
+            } else {
+                lo += 1;
+            }
+        }
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
---
+++ src/dp/max_product.rs
@@ -0,0 +1,29 @@
+/// # [152. 乘积最大子数组](https://leetcode.cn/problems/maximum-product-subarray/)
+///
+
+struct Solution;
+
+impl Solution {
+    //
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut pre_max = 1;
+        let mut pre_min = 1;
+        let mut ans = nums[0];
+        for num in nums {
+            let max = pre_max;
+            let min = pre_min;
+            pre_max = num.max((num * max).max(num * min));
+            pre_min = num.min((num * max).min(num * min));
+            ans = ans.max(pre_max);
+        }
+        ans
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {}
+}
