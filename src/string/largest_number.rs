28599d7bfbda9294617ec3d8efebcfbf03d894f1
wangyinhe <wyinhe@sina.cn>
2026-07-04 00:25:51 +0800

    最大数 特殊排序

---
+++ src/string/largest_number.rs
@@ -0,0 +1,25 @@
+/// # [179. 最大数](https://leetcode.cn/problems/largest-number/)
+///
+
+pub struct Solution;
+impl Solution {
+    pub fn largest_number(mut nums: Vec<i32>) -> String {
+        let mut strs: Vec<String> = nums.into_iter().map(|num| num.to_string()).collect();
+        strs.sort_unstable_by(|a, b| {
+            let ab = a.as_bytes().iter().chain(b.as_bytes());
+            let ba = b.as_bytes().iter().chain(a.as_bytes());
+            ba.cmp(ab)
+        });
+
+        if strs[0] == "0" {
+            return String::from("0");
+        }
+        strs.concat()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test1() {}
+}
--- src/string/mod.rs
+++ src/string/mod.rs
@@ -1,6 +1,7 @@
 pub mod character_replacement;
 pub mod find_anagrams;
 pub mod group_anagrams;
+pub mod largest_number;
 pub mod length_of_longest_substring;
 pub mod longest_common_prefix;
 pub mod min_window;
