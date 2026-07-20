e59d73315dc242c08af7fab1aa29149d52776dd1
wangyinhe <wyinhe@sina.cn>
2026-07-20 15:45:38 +0800

    Morris中序遍历

--- dsa/src/binary_tree/binary_search_tree/bst_iterator.rs
+++ dsa/src/binary_tree/binary_search_tree/bst_iterator.rs
@@ -3,6 +3,8 @@
 use crate::binary_tree::TreeNode;
 use std::cell::RefCell;
 use std::rc::Rc;
+
+/// 用栈存储数据状态，栈顶永远放着下一个要遍历到的节点
 pub struct BSTIterator {
     stack: Vec<Rc<RefCell<TreeNode>>>,
 }
@@ -32,5 +34,50 @@ impl BSTIterator {
 
     pub fn has_next(&self) -> bool {
         !self.stack.is_empty()
+    }
+}
+
+/// 转化为链表，每次调用next都直接消耗头节点
+pub struct BSTIterator2 {
+    head: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl BSTIterator2 {
+    pub fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
+        let dummy = Rc::new(RefCell::new(TreeNode::new(-1)));
+        let mut prev = dummy.clone();
+        let mut visitor = root;
+        while let Some(visitor_rc) = visitor {
+            let left = visitor_rc.borrow_mut().left.take();
+            if let Some(left_rc) = left {
+                // 当前节点挂到left上
+                let mut p = left_rc.clone();
+                while p.borrow().right.is_some() {
+                    let next = p.borrow().right.clone();
+                    p = next.unwrap();
+                }
+                p.borrow_mut().right = Some(visitor_rc);
+                visitor = Some(left_rc);
+            } else {
+                prev.borrow_mut().right = Some(visitor_rc.clone());
+                visitor = visitor_rc.borrow().right.clone();
+                prev = visitor_rc;
+            }
+        }
+        Self {
+            head: dummy.borrow_mut().right.take(),
+        }
+    }
+
+    pub fn next(&mut self) -> i32 {
+        if let Some(node_rc) = self.head.take() {
+            self.head = node_rc.borrow_mut().right.take();
+            return node_rc.borrow().val;
+        }
+        unreachable!()
+    }
+
+    pub fn has_next(&self) -> bool {
+        self.head.is_some()
     }
 }
